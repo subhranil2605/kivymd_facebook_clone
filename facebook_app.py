76b146ce5af727211da4752db57609f5fed22026
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2021-05-24 19:32:15 +0530

    changing 'd' to 'friend_name'

--- facebook_app.py
+++ facebook_app.py
@@ -21,32 +21,32 @@ class FacebookApp(MDScreen):
     def list_online_friends(self):
         with open("assets/online_friends.json") as f_obj:
             data = json.load(f_obj)
-            for friends in data:
+            for friend_name in data:
                 self.ids.online_friends.add_widget(OnlineAvatarImage(
-                    avatar=data[friends]['avatar']
+                    avatar=data[friend_name]['avatar']
                 ))
 
     def list_stories(self):
         with open("assets/stroies.json") as f_obj:
             data = json.load(f_obj)
-            for d in data:
+            for friend_name in data:
                 self.ids.stories.add_widget(StoryCard(
-                    name=d,
-                    image=data[d]["image"],
-                    avatar=data[d]["avatar"]
+                    name=friend_name,
+                    image=data[friend_name]["image"],
+                    avatar=data[friend_name]["avatar"]
                 ))
 
     def list_posts(self):
         with open("assets/posts.json") as f_obj:
             data = json.load(f_obj)
-            for d in data:
+            for friend_name in data:
                 self.ids.timeline.add_widget(Post(
-                    name=d,
-                    avatar=data[d]["avatar"],
-                    post=data[d]["post"],
+                    name=friend_name,
+                    avatar=data[friend_name]["avatar"],
+                    post=data[friend_name]["post"],
 
-                    likes=int(data[d]["likes"]),
-                    comments=int(data[d]["comments"]),
-                    caption=data[d]["caption"],
-                    updated_ago=data[d]["updated_ago"]
+                    likes=int(data[friend_name]["likes"]),
+                    comments=int(data[friend_name]["comments"]),
+                    caption=data[friend_name]["caption"],
+                    updated_ago=data[friend_name]["updated_ago"]
                 ))
